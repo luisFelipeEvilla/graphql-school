04e1929014dcbe540765b0e27c9d32e4e9dd62ea
Luis Felipe Evilla <luisfelipe588@hotmail.com>
2019-07-03 11:15:36 -0500

    integration with mongo db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+require('dotenv').config()
 const express = require('express');
 const gqlMiddleware = require('express-graphql');
 const { makeExecutableSchema } = require('graphql-tools');
